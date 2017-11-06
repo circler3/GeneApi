4852fd3decdcc1edea78e17cf9c934cbed1b1f92
circler3 <circler3@sina.com>
2017-11-06 19:09:40 +0800

    Initial commit

---
+++ GeneApi/Controllers/FileController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Net.Http.Headers;
+using OfficeOpenXml;
+
+namespace GeneApi.Controllers
+{
+    [Produces("application/json")]
+    [Consumes("application/json", "multipart/form-data")]
+    [Route("api/[controller]")]
+    public class FileController : Controller
+    {
+        // GET: api/File
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/File/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+        // POST: api/File
+        [HttpPost]
+        public async Task<ActionResult> Post(IFormFile file)
+        {
+            var stream = file.OpenReadStream();
+            var name = file.FileName;
+            using (ExcelPackage excel = new ExcelPackage(stream))
+            {
+                try
+                {
+                    ExcelWorksheet worksheet = package.Workbook.Worksheets[1];
+                    int rowCount = worksheet.Dimension.Rows;
+                    int ColCount = worksheet.Dimension.Columns;
+                    bool bHeaderRow = true;
+                    for (int row = 1; row <= rowCount; row++)
+                    {
+                        for (int col = 1; col <= ColCount; col++)
+                        {
+                            if (bHeaderRow)
+                            {
+                                sb.Append(worksheet.Cells[row, col].Value.ToString() + "\t");
+                            }
+                            else
+                            {
+                                sb.Append(worksheet.Cells[row, col].Value.ToString() + "\t");
+                            }
+                        }
+                        sb.Append(Environment.NewLine);
+                    }
+                    return Content(sb.ToString());
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+                return null;
+            }
+        }
+        
+        // PUT: api/File/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ GeneApi/Models/DataItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeneApi.Models
+{
+    public class DataItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
